36ea14c2b06752669c790dd168322f9da26b798b
sushma07662 <sushma.panduri@godigit.com>
2024-08-20 14:11:10 +0530

    added header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  REFRESH_ICON,
+  SETTING_ICON,
+  MENU_ICON,
+  IMG_ICON,
+  COLOR_PALATTE_ICON,
+  LIST_VIEW_ICON,
+  OTHER_MENU_ICON,
+  SEARCH_ICON,
+  MORE_ICON,
+  MENU_ICON1
+} from '../../assets/svg.icons';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral(
+      'refresh-icon',
+      sanitizer.bypassSecurityTrustHtml(REFRESH_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'setting-icon',
+      sanitizer.bypassSecurityTrustHtml(SETTING_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'menu-icon',
+      sanitizer.bypassSecurityTrustHtml(MENU_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'img-icon',
+      sanitizer.bypassSecurityTrustHtml(IMG_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'color-palatte-icon',
+      sanitizer.bypassSecurityTrustHtml(COLOR_PALATTE_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'list-view-icon',
+      sanitizer.bypassSecurityTrustHtml(LIST_VIEW_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'other-menu-icon',
+      sanitizer.bypassSecurityTrustHtml(OTHER_MENU_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'search-icon',
+      sanitizer.bypassSecurityTrustHtml(SEARCH_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'more-icon',
+      sanitizer.bypassSecurityTrustHtml(MORE_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'menu-icon1',
+      sanitizer.bypassSecurityTrustHtml(MENU_ICON1)
+    );
+  }
 
+  ngOnInit(): void {}
 }
