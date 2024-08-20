a02e78dd38d5c1f047b7199bb84f43644d468a87
sushma07662 <sushma.panduri@godigit.com>
2024-08-20 14:16:57 +0530

    added dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { NOTE_ICON1, CHECKBOX, IMAGE, BRUSH_ICON} from 'src/assets/svg.icons';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  noteIcon1 = NOTE_ICON1;
+  checkbox = CHECKBOX;
+  image = IMAGE;
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral(
+      'brush-icon',
+      sanitizer.bypassSecurityTrustHtml(BRUSH_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'image',
+      sanitizer.bypassSecurityTrustHtml(IMAGE)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'checkbox',
+      sanitizer.bypassSecurityTrustHtml(CHECKBOX)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'noteicon1',
+      sanitizer.bypassSecurityTrustHtml(NOTE_ICON1)
+    );
+  }
+
+  ngOnInit(): void {
+  }
 
 }
